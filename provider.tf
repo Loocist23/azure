64b1a4dbd984c68d9692c378a754fd3f816a14cd
Loocist <anthonyzegnal05@gmail.com>
2023-12-05 13:05:41 +0100

    on retest

--- provider.tf
+++ provider.tf
@@ -1,14 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
-
-}
-
-provider "azurerm" {
-    features {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
+  }
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = ".kube/config"
-  }
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
